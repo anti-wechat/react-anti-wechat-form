341516ca62beb8038c4d7b2a4ae804f6336fb71a
littlee <usedlittlee@gmail.com>
2018-12-20 15:16:41 +0800

    finish first version

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # react-robust-form-controls
+
 create robust form controls, which never let you down on iOS 12.1+ &amp; WeChat 6.7.4+
+
+## Problem
+
+![demo](assets/demo_1.png);
+
+## Usage
+
+simply wrap all form controls that will trigger keyboard with `robust()` provided by `<RobustFormControl />`
+
+```
+npm i -S react-robust-form-controls
+```
+
+```js
+import React from 'react';
+import RobustFormControls from 'react-robust-form-controls';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <RobustFormControls>
+          {robust => (
+            <div>
+              {robust(<input type="text" />)}
+              {robust(
+                <select>
+                  <option value="1">pick me</option>
+                  <option value="2">pick me</option>
+                </select>
+              )}
+              {robust(<textarea />)}
+            </div>
+          )}
+        </RobustFormControls>
+      </div>
+    );
+  }
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log('ss')
+import React from 'react';
+
+class RobustFormControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.hasFocus = false;
+    this.scrollPos = {
+      top: 0,
+      left: 0
+    };
+  }
+
+  robust = elem => {
+    return React.cloneElement(elem, {
+      onFocus: (...args) => {
+        this._focus(...args);
+        elem.props.onFocus && elem.props.onFocus(...args);
+      },
+      onBlur: (...args) => {
+        this._blur(...args);
+        elem.props.onBlur && elem.props.onBlur(...args);
+      }
+    });
+  };
+
+  _focus = () => {
+    this.scrollPos = {
+      top: document.documentElement.scrollTop,
+      left: document.documentElement.scrollLeft
+    };
+    this.hasFocus = true;
+  };
+
+  _blur = () => {
+    this.hasFocus = false;
+    setTimeout(() => {
+      if (!this.hasFocus) {
+        // havn't figure out the perfect solution, hit me a pull request
+        // document.documentElement.scrollTop = this.scrollPos.top;
+        // document.documentElement.scrollLeft = this.scrollPos.left;
+        // document.documentElement.scrollTop = 0;
+        // document.documentElement.scrollLeft = 0;
+        window.scrollTo(0, 0);
+      }
+    }, 100);
+  };
+
+  render() {
+    return this.props.children && this.props.children(this.robust);
+  }
+}
+
+export default RobustFormControl;
